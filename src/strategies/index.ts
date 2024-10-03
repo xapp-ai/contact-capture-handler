c713e2f3d38bfbac7ea40a74351fe5cf19ab60e1
Michael Myers <michaelmyers@users.noreply.github.com>
2024-10-03 18:01:28 -0400

    fix: Export Form Fallback Function (#447)

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -1,5 +1,8 @@
 /*! Copyright (c) 2023, XAPP AI */
 
+export * from "./FormResponseStrategy";
 export * from "./ProgrammaticResponseStrategy";
 export * from "./ResponseStrategy";
 export * from "./ResponseStrategySelector";
+
+export * from "./utils/forms";
